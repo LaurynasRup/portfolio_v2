5b9982b740e50e0a6f76b276aee95a60045aa877
Laurynas Rupainis <laurynas@thisisbliss.com>
2023-10-29 12:38:32 +0000

    Mobile work

---
+++ components/HeroSection.tsx
@@ -0,0 +1,22 @@
+import styles from '../styles/HeroSection.module.scss';
+import DotsGraphic from './DotsGraphic';
+
+export default function HeroSection() {
+  return (
+    <section className={styles.heroSection}>
+      <div className={styles.titleWrapper}>
+        <h1 className={styles.pageTitle}>
+          {['Modern', 'Web', 'Development', 'Solutions'].map((word, idx) => (
+            <span key={idx} className={styles.pageTitleSpanOuter}>
+              <span className={styles.pageTitleSpanInner}>{word}&nbsp;</span>
+            </span>
+          ))}
+        </h1>
+        <p className={styles.pageSubtitle}>by Laurynas Rupainis</p>
+      </div>
+      <div className={styles.imageWrapper}>
+        <DotsGraphic width={732} height={465} />
+      </div>
+    </section>
+  );
+}
